fac148ae2ac33cecb4f8085384d6b228d3eeb432
Karl R <erkarl@gmail.com>
2012-10-29 19:12:24 +0200

    Added twitter4j lib

--- src/com/krsoft/piqu/MainActivity.java
+++ src/com/krsoft/piqu/MainActivity.java
@@ -3,6 +3,7 @@ package com.krsoft.piqu;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import twitter4j.*;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,18 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        String twitterID = "blabla@gmail.com";
+        String twitterPassword = "blabla";
+        
+        Twitter twitter = new Twitter(twitterID,twitterPassword);
+        List statuses = twitter.getFriendsTimeline();
+        System.out.println("Showing friends timeline.");
+        for(int i=0; i < statuses.size() ; i++) {
+          Status status = (Status)statuses.get(i);
+            System.out.println(status.getUser().getName() + ":" +
+                               status.getText()); 
+        }
     }
 
     @Override
