82ed6ea5832f3e0891a5e07e4b09c09e4c98d43b
Karl R <erkarl@gmail.com>
2012-11-25 18:27:38 +0200

    Minor change to LogutActivity

--- src/com/krsoft/piqu/activity/LogoutActivity.java
+++ src/com/krsoft/piqu/activity/LogoutActivity.java
@@ -1,15 +1,14 @@
 package com.krsoft.piqu.activity;
 
-import com.krsoft.piqu.R;
-import com.krsoft.piqu.data.Constants;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
 
-public class LogoutActivity extends Activity {
+import com.krsoft.piqu.R;
+import com.krsoft.piqu.data.Constants;
+
+public class LogoutActivity extends BaseActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
