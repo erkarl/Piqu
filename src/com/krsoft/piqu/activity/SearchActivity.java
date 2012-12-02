e9cc4500dc81de018a59a69dd460d384d8f76c87
Karl R <erkarl@gmail.com>
2012-12-02 23:06:08 +0200

    Fixed #22 - the application no redirects to login activity when you try to tweet/search while logged out

--- src/com/krsoft/piqu/activity/SearchActivity.java
+++ src/com/krsoft/piqu/activity/SearchActivity.java
@@ -1,6 +1,7 @@
 package com.krsoft.piqu.activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -16,20 +17,41 @@ public class SearchActivity extends BaseActivity {
 	private OnClickListener searchButtonListener = new OnClickListener() {
 
 		public void onClick(View v) {
-			EditText searchEditText = (EditText) findViewById(R.id.searchEditText);
-			String searchText = searchEditText.getText().toString();
-			if ("".equals(searchText)) {
-				Toast.makeText(SearchActivity.this, R.string.emptySearch,
-						Toast.LENGTH_SHORT).show();
+			if (!(isLoggedIn())) {
+				Intent mainActivity = new Intent(SearchActivity.this,
+						MainActivity.class);
+				startActivity(mainActivity);
 			} else {
-				Intent searchActivityResults = new Intent(SearchActivity.this,
-						SearchResultsActivity.class);
-				searchActivityResults.putExtra(Constants.INTENT_EXTRA_SEARCH_QUERY, searchText);
-				startActivity(searchActivityResults);
+				EditText searchEditText = (EditText) findViewById(R.id.searchEditText);
+				String searchText = searchEditText.getText().toString();
+				if ("".equals(searchText)) {
+					Toast.makeText(SearchActivity.this, R.string.emptySearch,
+							Toast.LENGTH_SHORT).show();
+				} else {
+					Intent searchActivityResults = new Intent(
+							SearchActivity.this, SearchResultsActivity.class);
+					searchActivityResults.putExtra(
+							Constants.INTENT_EXTRA_SEARCH_QUERY, searchText);
+					startActivity(searchActivityResults);
+				}
 			}
 
 		}
 	};
+
+	public boolean isLoggedIn() {
+		SharedPreferences pref = getSharedPreferences(Constants.PREF_NAME,
+				MODE_PRIVATE);
+		String accessToken = pref.getString(Constants.PREF_KEY_ACCESS_TOKEN,
+				null);
+		String accessTokenSecret = pref.getString(
+				Constants.PREF_KEY_ACCESS_TOKEN_SECRET, null);
+		if (accessToken == null || accessTokenSecret == null) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
--- src/com/krsoft/piqu/activity/TweetActivity.java
+++ src/com/krsoft/piqu/activity/TweetActivity.java
@@ -16,40 +16,60 @@ import com.krsoft.piqu.data.Constants;
 import com.krsoft.piqu.network.TweetActivityTask;
 import com.krsoft.piqu.util.TweetActivityResultsListener;
 
-public class TweetActivity extends BaseActivity implements TweetActivityResultsListener {
+public class TweetActivity extends BaseActivity implements
+		TweetActivityResultsListener {
 
 	private OnClickListener tweetButtonListener = new OnClickListener() {
 
 		@SuppressWarnings("unchecked")
 		public void onClick(View v) {
-			EditText statusText = (EditText) findViewById(R.id.tweetEditText);
-			String tweetText = statusText.getText().toString();
-			if ("".equals(tweetText)) {
-				Toast.makeText(TweetActivity.this, R.string.tweetFail,
-						Toast.LENGTH_LONG).show();
-			} 
-			else if (tweetText.length() > Constants.MAX_TWEET_LENGTH) {
-				Toast.makeText(TweetActivity.this, R.string.tweetLengthFail,
-						Toast.LENGTH_LONG).show();
-			}
-			else {
-				SharedPreferences pref = getSharedPreferences(
-						Constants.PREF_NAME, MODE_PRIVATE);
-				String accessToken = pref.getString(
-						Constants.PREF_KEY_ACCESS_TOKEN, null);
-				String accessTokenSecret = pref.getString(
-						Constants.PREF_KEY_ACCESS_TOKEN_SECRET, null);
-				ArrayList<String> params = new ArrayList<String>();
-				params.add(accessToken);
-				params.add(accessTokenSecret);
-				params.add(tweetText);
-				TweetActivityTask tweetActivityTask = new TweetActivityTask();
-				tweetActivityTask.setOnResultsListener(TweetActivity.this);
-				tweetActivityTask.execute(params);
-			}
+			if (!(isLoggedIn())) {
+				Intent mainActivity = new Intent(TweetActivity.this,
+						MainActivity.class);
+				startActivity(mainActivity);
+			} else {
+				EditText statusText = (EditText) findViewById(R.id.tweetEditText);
+				String tweetText = statusText.getText().toString();
+				if ("".equals(tweetText)) {
+					Toast.makeText(TweetActivity.this, R.string.tweetFail,
+							Toast.LENGTH_LONG).show();
+				} else if (tweetText.length() > Constants.MAX_TWEET_LENGTH) {
+					Toast.makeText(TweetActivity.this,
+							R.string.tweetLengthFail, Toast.LENGTH_LONG).show();
+				} else {
+					SharedPreferences pref = getSharedPreferences(
+							Constants.PREF_NAME, MODE_PRIVATE);
+					String accessToken = pref.getString(
+							Constants.PREF_KEY_ACCESS_TOKEN, null);
+					String accessTokenSecret = pref.getString(
+							Constants.PREF_KEY_ACCESS_TOKEN_SECRET, null);
+					ArrayList<String> params = new ArrayList<String>();
+					params.add(accessToken);
+					params.add(accessTokenSecret);
+					params.add(tweetText);
+					TweetActivityTask tweetActivityTask = new TweetActivityTask();
+					tweetActivityTask.setOnResultsListener(TweetActivity.this);
+					tweetActivityTask.execute(params);
+				}
 
+			}
 		}
+
 	};
+
+	public boolean isLoggedIn() {
+		SharedPreferences pref = getSharedPreferences(Constants.PREF_NAME,
+				MODE_PRIVATE);
+		String accessToken = pref.getString(Constants.PREF_KEY_ACCESS_TOKEN,
+				null);
+		String accessTokenSecret = pref.getString(
+				Constants.PREF_KEY_ACCESS_TOKEN_SECRET, null);
+		if (accessToken == null || accessTokenSecret == null) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -64,7 +84,8 @@ public class TweetActivity extends BaseActivity implements TweetActivityResultsL
 		Toast.makeText(this, R.string.tweetSuccess, Toast.LENGTH_LONG).show();
 		Intent twitterFeedActivityIntent = new Intent(this,
 				TwitterFeedActivity.class);
-		twitterFeedActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+		twitterFeedActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+				| Intent.FLAG_ACTIVITY_SINGLE_TOP);
 		startActivity(twitterFeedActivityIntent);
 	}
 
